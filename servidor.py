75c8aac3bbce4a4072b421e2a78f4f4b5aa1f933
José Pach <nimda@MacBook-Pro-de-Jose.local>
2018-11-16 01:45:01 -0600

    Progress: socket udp

--- servidor.py
+++ servidor.py
@@ -2,18 +2,32 @@
 
 import socket
 import select
+import threading
 import json
 
 server = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind( ('localhost', 10000) )
 server.listen( 3 )
+server.setblocking(0)
 
 toread = [server]
 
 running = 1
 
 a = [ 'Jose' ]
+
+def sendList():
+	MCAST_GRP = '224.1.1.1'
+	MCAST_PORT = 5007
+	while 1:
+		try:
+			sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+			sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
+			sock.sendto('Hello World!'.encode(), (MCAST_GRP, MCAST_PORT))
+			print( "Enviando copia vía multicast" )
+		except:
+			print( "No es posible enviar copiar de la lista" )
 
 def recvall(sock):
     BUFF_SIZE = 1024 # 4 KiB
@@ -54,84 +68,39 @@ def option( socket, option ):
 		print( 'index:' , index )
 	else: print( 'opción desconocida', option )
 
-# we will shut down when all clients disconenct                                                                                                                                                                                                                      
-while 1:
+def runServer():
+	while 1:
+		rready,wready,err = select.select( toread, [], [] )
+		for s in rready:
+			if s == server:
+				# accepting the socket, which the OS passes off to another                                                                                                                                                                                               
+				# socket so we can go back to selecting.  We'll append this                                                                                                                                                                                              
+				# new socket to the read list we select on next pass                                                                                                                                                                                                     
 
-	rready,wready,err = select.select( toread, [], [] )
-	for s in rready:
-		if s == server:
-			# accepting the socket, which the OS passes off to another                                                                                                                                                                                               
-			# socket so we can go back to selecting.  We'll append this                                                                                                                                                                                              
-			# new socket to the read list we select on next pass                                                                                                                                                                                                     
-
-			client, address = server.accept()
-			toread.append( client )  # select on this socket next time                                                                                                                                                                                               
-		else:
-			# Not the server's socket, so we'll read                                                                                                                                                                                                                 
-			data = s.recv( 1024 )
-			if data:
-				print( "Received %s" % ( data  ) )
-				option( s, data.decode() )
+				client, address = server.accept()
+				toread.append( client )  # select on this socket next time                                                                                                                                                                                               
 			else:
-				print( "Client disconnected" )
-				s.close()
+				# Not the server's socket, so we'll read                                                                                                                                                                                                                 
+				data = s.recv( 1024 )
+				if data:
+					print( "Received %s" % ( data  ) )
+					option( s, data.decode() )
+				else:
+					print( "Client disconnected" )
+					s.close()
 
-				# remove socket so we don't watch an invalid 
-				# descriptor, decrement client count                                                                                                                                                                      
-				toread.remove( s )
-				running = len(toread) - 1
+					# remove socket so we don't watch an invalid 
+					# descriptor, decrement client count                                                                                                                                                                      
+					# toread.remove( s )
+					# running = len(toread) - 1
 
-# clean up                                                                                                                                                                                                                                                           
-server.close()
-
-# import socket
-# import json
-# from threading import *
-
-
-# HOST = '127.0.0.1'                 # Symbolic name meaning the local host
-# PORT = 10000 
-# # serversocket.bind((host, port))
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.bind((HOST, PORT))
-# a = { 'id': '1234' }
+	# clean up                                                                                                                                                                                                                                                           
+	server.close()
 
 
-# class client(Thread):
-# 	def __init__(self, socket, address):
-# 		Thread.__init__(self)
-# 		self.sock = socket
-# 		self.addr = address
-# 		self.start()
-
-# 	def run(self):
-# 		while 1:
-# 			data = self.sock.recv(1024).decode()
-# 			print('Client sent:', data )
-# 			self.option( data )
-# 			break
-
-# 		self.sock.close()
-# 			# self.sock.send(b'Oi you sent something to me')
-
-# 	def option( self, option ):
-# 		if option == '1':
-# 			data = json.dumps(a)
-# 			self.sock.sendall( data.encode('utf-8') )
-# 			print( 'Enviando ', data )
-# 		elif option == '2':
-# 			# recibir nuevamente
-# 			print( 'Añadiendo ')
-# 		elif option == '3':
-# 			# recibir nuevamente
-# 			print( 'Editando ')
-
-			
-
-# s.listen(3)
-
-# print ('server started and listening')
-# while 1:
-# 	conn, addr = s.accept()
-# 	print( 'Connected by', addr )
-# 	client(conn, addr)
+sendList()
+# try:
+# 	# threading.Thread(target=runServer).start()
+# 	# threading.Thread(target=).start()
+# except:
+#    print ("Error: unable to start thread")
